3755f2f6803e0b84fc124c8e2b1c6acbdbac3e6b
emalema <eyemalema5@gmail.com>
2023-07-19 14:57:00 +0900

    ユーザー認証機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -13,7 +13,7 @@
 ActiveRecord::Schema.define(version: 2023_07_19_040620) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname", null: false
+    t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
